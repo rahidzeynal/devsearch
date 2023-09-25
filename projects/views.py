8bd0dcf5b37dd89e0c70b74858a4964d91ef6711
Rahid Zeynalov <zeynalovrahid1@gmail.com>
2023-09-25 18:24:23 +0400

    Test

--- projects/views.py
+++ projects/views.py
@@ -19,34 +19,43 @@ def project(request, pk):
 
 @login_required(login_url="login")
 def createProject(request):
+    profile = request.user.profile
+
     form = ProjectForm()
 
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
+            messages.success(request, 'Project was created successfully!')
             return redirect('projects')
     context = {'form':form}
     return render(request, 'projects/project_form.html', context=context)
 
 @login_required(login_url="login")
 def updateProject(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     form = ProjectForm(instance=project)
 
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Project was updated successfully!')
             return redirect('projects')
     context = {'form':form}
     return render(request, 'projects/project_form.html', context=context)
 
 @login_required(login_url="login")
 def deleteProject(request, pk):
-    project=Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     if request.method == 'POST':
         project.delete()
+        messages.success(request, 'Project was deleted successfully!')
         return redirect('projects')
     context = {'object':project}
-    return render(request, 'projects/delete_template.html', context=context)
+    return render(request, 'delete_template.html', context=context)
